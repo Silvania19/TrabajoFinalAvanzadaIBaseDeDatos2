95ad495a20b2b02d5984df188887f365b45b2c15
ignacio chiaradia <ignachy01@gmail.com>
2021-06-07 02:27:42 -0300

    two tests of billController added

--- src/main/java/com/utn/TPfinal/controller/BillController.java
+++ src/main/java/com/utn/TPfinal/controller/BillController.java
@@ -20,8 +20,12 @@ import java.util.List;
 @RequestMapping("/bill")
 public class BillController {
 
-    @Autowired
     BillService billService;
+
+    @Autowired
+    public BillController(BillService billService){
+        this.billService = billService;
+    }
 
     //2) Consulta de facturas por rango de fechas.
     @GetMapping()
